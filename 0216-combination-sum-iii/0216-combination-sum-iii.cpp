cbc54b4d57013cf93a3fddfaaa2c0074bff8281d
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2025-08-28 03:01:36 +0530

    Time: 0 ms (100%), Space: 10.1 MB (13.58%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     vector<vector<int>>res;
-    void solve(int k_no, int sum, vector<int>temp, int id){
-        if(temp.size()==k_no && sum==0) res.push_back(temp);
-        if(temp.size() > k_no || sum<0) return;
+    void solve(int target, int len, vector<int>temp, int id){
+        if(target <0|| len<0) return;
+        if(target==0 && len==0) {
+            res.push_back(temp);
+            return;
+        }
         for(int i=id; i<=9; i++){
             temp.push_back(i);
-            solve(k_no, sum-i, temp, i+1);
+            solve(target-i, len-1, temp , i+1);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
+        //k=number int, n=sum
         vector<int>temp;
-        solve(k, n, temp, 1);
+        solve(n, k, temp, 1);
         return res;
     }
 };
