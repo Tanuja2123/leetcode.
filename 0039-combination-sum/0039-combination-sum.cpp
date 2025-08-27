a5432f4cc49777cfa92d21a22b8128cbfcef0442
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2025-08-28 02:49:15 +0530

    Time: 28 ms (19.35%), Space: 21 MB (20.12%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<vector<int>>res;
     int n;
-    void solve(vector<int>& c, int target, vector<int>&temp, int id){
+    void solve(vector<int>&c, int target, vector<int>temp, int id){
         if(target<0)return;
-        if(target == 0) res.push_back(temp);
+        if(target==0) res.push_back(temp);
         for(int i=id; i<n; i++){
             temp.push_back(c[i]);
             solve(c, target-c[i], temp, i);
@@ -14,7 +14,7 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         n=candidates.size();
         vector<int>temp;
-        solve(candidates, target, temp, 0);
+        solve(candidates,target,temp, 0);
         return res;
     }
 };
