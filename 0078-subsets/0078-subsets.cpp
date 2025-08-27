36a9c48523a0da9043821d25266fcb435531d19b
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2025-08-28 00:40:50 +0530

    Time: 0 ms (100%), Space: 10.3 MB (35.77%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    // backtracking solution
     vector<vector<int>> res;
     int n;
-    void solve(vector<int>& nums, int id, vector<int>& temp) {
-       res.push_back(temp);
-       for(int i=id; i<n; i++){
+    void solve(vector<int> temp, vector<int>& nums, int id) {
+        res.push_back(temp);
+        for (int i = id; i < n; i++) {
             temp.push_back(nums[i]);
-            solve(nums, i+1, temp);
+            solve(temp, nums, i+1);
             temp.pop_back();
-       }
+        }
     }
-
-
     vector<vector<int>> subsets(vector<int>& nums) {
         n = nums.size();
-        res.clear();
         vector<int> temp;
-        solve(nums, 0, temp);
+        solve(temp, nums, 0);
         return res;
     }
 };
