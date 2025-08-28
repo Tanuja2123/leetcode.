bc06575072409af998ec4591ddeb1ee95605622f
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2025-08-29 03:49:32 +0530

    Time: 3 ms (34.49%), Space: 10.9 MB (33.21%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
+    vector<string>res;
     int n;
-    vector<string> res;
-    void find(string& s, int id, int part, string curr) {
-        if (part == 4) {
-            if (id == n) {
+    void solve(string s, int id, int part, string curr){
+         if(part==4){
+            if(id==n){
                 curr.pop_back();
                 res.push_back(curr);
             }
             return;
         }
-        for (int len = 1; len <= 3; len++) { // 3 len max possible
-            if (id + len > n)
-                break;
-            string segment = s.substr(id, len); // substr of len staring form id
-            int val = stoi(segment);
-            if (segment[0] == '0' && len>1 || val > 255) // len greater than 1
-                continue;
-            find(s, id + len, part + 1, curr + segment + ".");
+        for(int len=1; len<=3; len++){
+            if(id+len >n) break;
+
+            string seg= s.substr(id, len);
+            int val=stoi(seg);
+            if(seg[0]=='0' && len>1 || val>255) continue;
+            solve(s, id+len, part+1, curr+seg+'.');
         }
     }
     vector<string> restoreIpAddresses(string s) {
-        n = s.length();
-        find(s, 0, 0, ""); // string s, id, parts, curr
+        n=s.length();
+        solve(s, 0, 0, "");
         return res;
+       
     }
 };
