721515123f71f5a448c04220543df5576f8db40a
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2025-08-29 03:59:48 +0530

    Time: 74 ms (20.93%), Space: 84 MB (21.18%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-vector<vector<string>>res;
-int n;
-bool ispali(string str){
-    int l=0; int r=str.length()-1;
-    while(l<r){
-        if(str[l]==str[r]){
-            l++; r--;
-        } else{
-            return false;
+    vector<vector<string>>res;
+    int n;
+    bool ispali(string str){
+        int l=0, r=str.length()-1;
+        while(l<r){
+            if(str[l]==str[r]){
+                l++; r--;
+            } else{
+                return false;
+            }
+        }
+        return true;
+    }
+    void solve(string s, vector<string>& temp, int id){
+        if(id==n){
+            res.push_back(temp);
+            return ;
+        }
+        for(int i=id; i<n; i++){
+            if(ispali(s.substr(id, i-id+1))){ // id =start, len=i-id+1
+                temp.push_back(s.substr(id, i-id+1));
+                solve(s, temp, i+1);
+                temp.pop_back();
+            }
         }
     }
-    return true;
-    
-}
-void solve(string s, vector<string>&temp, int id){
-    if(id >=n){
-        res.push_back(temp);
-        return;
-    }
-    for(int i=id; i<n; i++){
-        if(ispali(s.substr(id, i-id+1))){ //start, len
-            temp.push_back(s.substr(id, i-id+1));
-            solve(s, temp, i+1);
-            temp.pop_back();
-        }
-    }
-}
-vector<vector<string>> partition(string s) {
+    vector<vector<string>> partition(string s) {
         n=s.length();
         vector<string>temp;
         solve(s, temp, 0);
         return res;
-
     }
 };
