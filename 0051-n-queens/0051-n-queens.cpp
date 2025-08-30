33ec7fd9dc2a90a10045293c869c5426fa1686f6
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2025-08-31 00:57:29 +0530

    Time: 9 ms (18.76%), Space: 16.2 MB (12.5%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int m;
     vector<vector<string>>res;
+    int n;
     vector<bool>col, dig, antidig;
-    void solve(vector<string>& temp, int r){
-        if(r==m){
+    void dfs( int r, vector<string>temp){
+        if(r==n){
             res.push_back(temp);
             return;
         }
-        for(int c=0; c<m; c++){
-            int d1= r+c, d2= r-c+m-1;
-            if(col[c]|| dig[d1] || antidig[d2]) continue;
-
-            col[c]=dig[d1]=antidig[d2]=true; temp[r][c]='Q';
-            solve(temp, r+1);
-            col[c]=dig[d1]=antidig[d2]=false; temp[r][c]='.';
+        for(int c=0; c<n; c++){
+            int d1= r+c, d2= r-c+n-1; //n-1 because the it starts from 0
+            if(col[c]|| dig[d1]|| antidig[d2]) continue;
+            col[c]=dig[d1]=antidig[d2]=true;
+            temp[r][c]='Q';
+            dfs(r+1, temp);
+            col[c]=dig[d1]=antidig[d2]=false;
+            temp[r][c]='.';
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        m=n;
-        vector<string>temp(m, string(m, '.'));
-        col.assign(m, false);
-        dig.assign(m*2-1, false);
-        antidig.assign(m*2-1, false);
-
-        solve(temp, 0); //row
+        this->n=n;
+        col.assign(n, false);
+        dig.assign(n*2-1, false);
+        antidig.assign(n*2-1, false);
+        int r=0;
+        vector<string>temp(n, string(n, '.'));
+        dfs( 0, temp);
         return res;
     }
 };
