e1cffb8b2f162599145cc0a5261b2aafb7424deb
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2025-09-03 01:59:34 +0530

    Time: 14 ms (30.41%), Space: 18.8 MB (95.91%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -6,29 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    unordered_map<long long ,int>presum;
-    int dfs(TreeNode* root, long long target, long long csum) {
-        if(!root) return 0;
-        csum += root->val;
-        int count= presum[csum-target];
-        //vacktrack
-        presum[csum]++;
-        count += dfs(root->left, target, csum);
-        count += dfs(root->right, target,csum); //target not subtracted in this case
-        presum[csum]--;
+    int solve(TreeNode* root, long long target){
+        if(!root) return 0 ;
+        int count=0;
+        if(target==root->val) count++;
+        count += solve(root->left, target-root->val);
+        count += solve(root->right, target-root->val);
         return count;
     }
-
     int pathSum(TreeNode* root, int targetSum) {
-        if (!root) return 0;
-        presum[0]=1;
-        // start from root + recurse on children
-        return dfs(root, targetSum, 0) ;
+        if(!root ) return 0;
+        //o(n^2) to call this function for all the children, left and right called for this function again
+        return solve(root, (long long )targetSum) + pathSum(root->right, targetSum) + pathSum(root->left, targetSum);
     }
 };
