7286da2fa112622e115875df41062c624bad608b
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2025-09-03 00:58:33 +0530

    Time: 17 ms (13.69%), Space: 40.7 MB (9.45%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -14,13 +14,15 @@ public:
     vector<vector<int>>res;
     void solve(TreeNode* root, int target, vector<int>path){
         if(!root) return;
+        //target <0 condition not here coz target can be negative
         path.push_back(root->val);
         if(!root->right && !root->left && target==root->val){
             res.push_back(path);
+            return;
         }
         solve(root->left, target-root->val, path);
         solve(root->right, target-root->val, path);
-        path.pop_back();
+        path.pop_back(); //pops 7 after reaching null,dead end 5,8,13 comes here to pop
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<int>path;
