e2d4ac56f5564a44369b31273a46f49e3e99a824
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2025-09-19 19:40:32 +0530

    Time: 0 ms (100%), Space: 29.4 MB (5.41%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -6,27 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-//maxsum returns the maxsum till the node maxsum
-    int maxsum;
-    int solve(TreeNode* root){
+    int ans=INT_MIN;
+    int solveh(TreeNode* root){
         if(!root) return 0;
-        int l=solve(root->left); //solve returns int
-        int r=solve(root->right); // 0 if doesn't exist
-        int niche_hi= l+r+root->val;
-        int koi_ek = max(l,r)+root->val;
-        int sirf_root= root->val;
-        maxsum=max({maxsum, niche_hi, koi_ek, sirf_root});
-        return max(sirf_root, koi_ek);
+        int l = solveh(root->left);
+        int r = solveh(root->right);
+        int nichehi= l+r+root->val;
+        int koiek= max(r, l)+ root->val;
+        int sroot= root->val;
+        ans= max({ans, nichehi, koiek, sroot});
+        return max(koiek, sroot); //carry back
+        
     }
     int maxPathSum(TreeNode* root) {
-       maxsum=INT_MIN;
-       solve(root);
-       return maxsum;
+        solveh(root);
+        return ans;
     }
 };
