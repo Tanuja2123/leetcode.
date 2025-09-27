f093496783e9e8cf833b03b041bd51cf868903c7
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2025-09-27 23:46:12 +0530

    Time: 0 ms (100%), Space: 12 MB (24.7%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,18 +11,20 @@
  */
 class Solution {
 public:
-    int solve(TreeNode * root, int csum){
+// approach - store the result of number formed at the leaf end of every node.
+//after all leaf nodes covered return the sum of l and r, which ultimately returns the sum of the all numbers formed at the leaf nodes
+    int solve(TreeNode* root, int csum){
         if(!root) return 0;
-        csum = 10*csum+root->val;
+        csum = 10*csum+ root->val;
         if(!root->left && !root->right){
-            return csum; // conti sum, name l and r
+            return csum;
         }
-        int l=solve(root->left, csum);
+        int l= solve(root->left, csum);
         int r=solve(root->right, csum);
         return l+r;
-
     }
     int sumNumbers(TreeNode* root) {
         return solve(root, 0);
+
     }
 };
