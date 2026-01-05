ae9ff1e7b11de91262e814dbf692c8848c887945
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2026-01-05 22:19:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (40.72%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0 || x %10 ==0 && x !=0) return false;
+        if(x<0 || x %10 ==0 && x !=0) return false; // negative, x multiple of x
         int reversed= 0;
         while(x> reversed){
             reversed= reversed*10 + x%10;
             x = x/10;
-        }
+        } // in the end rever>x or equal
         return x== reversed || x== reversed/10;
     }
 };
