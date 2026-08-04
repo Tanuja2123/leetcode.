d71b1d53a08f57f51b5366a9b5aebfb46a8ea922
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2026-08-04 22:59:38 +0530

    Time: 21 ms (73.01%), Space: 17.9 MB (84.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<int>dp(amount+1,1e9);
-        dp[0]=0; //base case
-        //dp[i]=number of coins required to make the sum of dp[i]
-        for(int i=1; i<=amount; i++){
-            for(int j=0; j<n; j++){
+        vector<int>dp(amount+1, 1e9);
+        // intialization: base case
+        dp[0]=0;
+        for(int i=1; i<=amount; i++){ // start from 1
+            for(int j=0; j<coins.size(); j++){
                 if(coins[j]<=i){
-                    dp[i]=min(dp[i], dp[i-coins[j]]+1);
+                    dp[i]= min(dp[i], dp[i-coins[j]]+1); //1 coin added, 
                 }
             }
         }
-        return (dp[amount] == 1e9) ? -1 : dp[amount];
+        return dp[amount]==1e9?-1:dp[amount];
     }
 };
