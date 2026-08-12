1bcd41654a831c0176ada872bd495f720b8acd39
Tanuja Sharma <124695608+Tanuja2123@users.noreply.github.com>
2026-08-12 22:56:17 +0530

    Time: 5 ms (54.43%), Space: 21.8 MB (5.3%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool toposort(unordered_map<int, vector<int>>adj, vector<int>indegree, int n){
+        queue<int>q;
+        int count=0;
+        for(int i=0; i<n; i++){
+            if(indegree[i]==0){
+                count++;
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int u=q.front(); q.pop();
+            for(int v: adj[u]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    count++;
+                    q.push(v);
+                }
+            }
+        }
+        if(count==n) return true;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>>adj;
+        vector<int>indegree(numCourses, 0);
+        for(auto &vec: prerequisites){
+            int a=vec[0];
+            int b=vec[1];
+            adj[b].push_back(a);
+            indegree[a]++;
+        }
+        return toposort(adj, indegree, numCourses);
+        
+    }
+};
